27293abde4b6209362e7ca0b89826e087a125121
Martin Vium <martin.vium@gmail.com>
2014-04-15 22:06:52 +0200

    Add first controller and rspec

---
+++ app/controllers/resource_controller.rb
@@ -0,0 +1,21 @@
+class ResourceController < ApplicationController
+  # GET /resource/get
+  def get
+    
+  end
+
+  # POST /resource/get
+  def post
+    
+  end
+
+  # PUT /resource/get
+  def put
+    
+  end
+
+  # DELETE /resource/get
+  def delete
+    
+  end
+end
