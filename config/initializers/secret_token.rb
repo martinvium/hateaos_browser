1554b8a66fa62c3dc15a808870544f1379f4095b
Martin Vium <martin.vium@gmail.com>
2014-04-15 21:49:02 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HateaosBrowser::Application.config.secret_key_base = '6ebde02040b62ca2d8d55b378f8a37d9eb714c39462ad87f26e6c942d5da8143d72f53775525abc9ca3aa743afe5a4528e4dabbe3d9c61b85de9da61b028cabf'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HateaosBrowser::Application.config.session_store :cookie_store, key: '_hateaos_browser_session'
